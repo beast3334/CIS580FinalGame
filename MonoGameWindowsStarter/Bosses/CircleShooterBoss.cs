05afb8de94c5028844f58c60f7fea6f6a6631c99
Nathan Miller <nathan232@ksu.edu>
2020-04-24 13:55:26 -0500

    Foundation for CircleShooterBoss

---
+++ MonoGameWindowsStarter/Bosses/CircleShooterBoss.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+namespace MonoGameWindowsStarter.Bosses
+{
+    public class CircleShooterBoss : Boss
+    {
+        BoundingRectangle bounds;
+        Texture2D texture;
+        Game1 game;
+        ContentManager content;
+
+        public override BoundingRectangle Bounds => throw new NotImplementedException();
+        public CircleShooterBoss(Game1 game, ContentManager content)
+        {
+            this.game = game;
+            this.content = content;
+        }
+
+        public override void LoadContent()
+        {
+            texture = content.Load<Texture2D>("CircleShooterBoss");
+            bounds.Height = 200;
+            bounds.Width = 200;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
