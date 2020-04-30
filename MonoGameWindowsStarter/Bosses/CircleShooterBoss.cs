d549c7b4d4efd371ac27c1181b6298d8978d3111
Nathan Miller <nathan232@ksu.edu>
2020-04-30 15:12:16 -0500

    Refresh

--- MonoGameWindowsStarter/Bosses/CircleShooterBoss.cs
+++ MonoGameWindowsStarter/Bosses/CircleShooterBoss.cs
@@ -24,7 +24,7 @@ namespace MonoGameWindowsStarter.Bosses
             this.content = content;
         }
 
-        public override void LoadContent()
+        public override void LoadContent(ContentManager content)
         {
             texture = content.Load<Texture2D>("CircleShooterBoss");
             bounds.Height = 200;
